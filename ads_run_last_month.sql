2314c924617fddfbd96af02d8e2c1beb74fd4c8d
Michael <alpha1@users.noreply.github.com>
2018-12-04 16:24:23 -0500

    Hid Dead Publications

--- ads_run_last_month.sql
+++ ads_run_last_month.sql
@@ -15,6 +15,7 @@ JOIN dim_publication ON sales_fact.pub_id=dim_publication.pub_id
 WHERE dim_order_item.dead = 0
 AND orders_fact.dead = 0
 AND sales_fact.dead = 0
+AND dim_publication.dead = 0
 AND dim_issue.issue_press_date BETWEEN DATE_FORMAT(NOW() - INTERVAL 1 MONTH, '%Y-%m-01 00:00:00') AND DATE_FORMAT(LAST_DAY(NOW() - INTERVAL 1 MONTH), '%Y-%m-%d 23:59:59')
 # Fill this out!
 AND dim_publication.name IN ('PUBLICATION_NAMES SEPARATED BY COMMAS')
