9a1c5d81f7af349ad4c8fc13ea6c8969de8f9891
magazinexperts <44814829+magazinexperts@users.noreply.github.com>
2020-01-10 13:37:32 -0500

    Update publication_yoy_report.sql

--- publication_yoy_report.sql
+++ publication_yoy_report.sql
@@ -3,8 +3,12 @@ dim_publication.name,
 #dim_issue.iss_name,
 DATE_FORMAT(dim_issue.issue_cover_date, '%M') 'Month/Year',
 #CONCAT("$",FORMAT(sum(sales_fact.net),2)) as 'Total Net',
-CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = "100" AND dim_issue.issue_cover_date BETWEEN "2018-01-01" AND "2019-01-01") THEN sales_fact.net ELSE 0 END),2)) AS '2018 Total Net 100%',
-CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = "100" AND dim_issue.issue_cover_date BETWEEN "2017-01-01" AND "2018-01-01") THEN sales_fact.net ELSE 0 END),2)) AS '2017 Total Net 100%',
+CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = 100 AND dim_issue.issue_cover_date BETWEEN "2020-01-01" AND "2020-12-31") THEN sales_fact.net ELSE 0 END),2)) AS '2020 Total Net 100%',
+CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability >= 90 AND dim_issue.issue_cover_date BETWEEN "2020-01-01" AND "2020-12-31") THEN sales_fact.net ELSE 0 END),2)) AS '2020 Total Net Above 90%',
+CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability >= 10 AND dim_issue.issue_cover_date BETWEEN "2020-01-01" AND "2020-12-31") THEN sales_fact.net ELSE 0 END),2)) AS '2020 Total Net Above 10%',
+CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = 100 AND dim_issue.issue_cover_date BETWEEN "2019-01-01" AND "2019-12-31") THEN sales_fact.net ELSE 0 END),2)) AS '2019 Total Net 100%',
+CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = 100 AND dim_issue.issue_cover_date BETWEEN "2018-01-01" AND "2019-12-31") THEN sales_fact.net ELSE 0 END),2)) AS '2018 Total Net 100%',
+CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = 100 AND dim_issue.issue_cover_date BETWEEN "2017-01-01" AND "2018-12-31") THEN sales_fact.net ELSE 0 END),2)) AS '2017 Total Net 100%',
 #CONCAT("$",FORMAT(SUM(CASE WHEN ( orders_fact.probability = "100" AND dim_issue.issue_cover_date BETWEEN "2016-01-01" AND "2017-01-01") THEN sales_fact.net ELSE 0 END),2)) AS '2016 Total Net 100%',
 #CONCAT("$",FORMAT(SUM(CASE WHEN orders_fact.probability > "90" THEN sales_fact.net ELSE 0 END),2)) AS 'Total Net > 90%',
 #CONCAT("$",FORMAT(SUM(CASE WHEN orders_fact.probability > "10" THEN sales_fact.net ELSE 0 END),2)) AS 'Total Net > 10%'
@@ -32,9 +36,8 @@ JOIN dim_adsize ON sales_fact.ad_size_id=dim_adsize.ad_size_id
 where orders_fact.dead = 0
 AND sales_fact.dead = 0
 AND dim_order_item.dead = 0
-AND dim_issue.issue_cover_date BETWEEN "2016-01-01" AND "2019-01-01"
+AND dim_issue.issue_cover_date BETWEEN "2016-01-01" AND "2020-12-31"
 #and orders_fact.probability = '100'
 #AND dim_publication.name = "Construction Executive"
 group by dim_publication.name, DATE_FORMAT(dim_issue.issue_cover_date, '%m') #WITH ROLLUP
 ORDER BY dim_publication.type DESC, dim_publication.name ASC, DATE_FORMAT(dim_issue.issue_cover_date, '%m') ASC
-
